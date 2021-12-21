42050c583eb5e659eecebda6d7f4a0830baf0a09
jasiel becerra <jasiel.csn@outlook.com>
2021-12-21 14:02:57 -0600

    Agregar archivos de proyecto.

---
+++ BlazorMusicCatalog.Data/AlbumMusicCatalogContext.cs
@@ -0,0 +1,20 @@
+﻿using BlazorMusicCatalog.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorMusicCatalog.Data
+{
+    public class AlbumMusicCatalogContext : DbContext
+    {
+        public DbSet<Album> Albums { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=LAPTOP-6ESMM72R\\SQLEXPRESS;Initial Catalog=AlbumMusicCatalog;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+    }
+}
---
+++ BlazorMusicCatalog.Data/Migrations/20210510185219_AddNumberToSongs.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlazorMusicCatalog.Data.Migrations
+{
+    public partial class AddNumberToSongs : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Number",
+                table: "Song",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Number",
+                table: "Song");
+        }
+    }
+}
